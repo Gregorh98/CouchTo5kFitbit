b33ed0ea5243ccac8e2495211c7010d7af043d21
Gregor <gregor.hastings@outlook.com>
2024-01-17 14:15:14 +0000

    Ability to click runs and get directed to correct run method

--- app/index.js
+++ app/index.js
@@ -1,3 +1,108 @@
-import { week_1 } from './runMethods.js'
+import { me as appbit } from "appbit";
+import { weekDistributor } from './runMethods.js'
+import { display } from "display";
 
-week_1();
+appbit.appTimeoutEnabled = false;
+//week_1();
+
+import document from "document";
+const tehs = document.getElementById("clickableForeground"); // TEHS - Touch-event handler screen (blame FitBit)
+const myLabel = document.getElementById("myLabel");
+
+const run1Button = document.getElementById("run1Button");
+const run2Button = document.getElementById("run2Button");
+const run3Button = document.getElementById("run3Button");
+
+
+var touchstartX = 0
+var touchendX = 0
+
+var currentWeekIndex = 0;
+var weeks = [
+{"name":"Week 1"},
+{"name":"Week 2"},
+{"name":"Week 3"},
+{"name":"Week 4"},
+{"name":"Week 5"},
+{"name":"Week 6"},
+{"name":"Week 7"},
+{"name":"Week 8"},
+{"name":"Week 9"}
+]
+
+myLabel.text = weeks[currentWeekIndex]["name"]
+
+function isWithinBounds(coords, bounds) {
+  return (
+    coords.x >= bounds[0] &&
+    coords.x <= bounds[2] &&
+    coords.y >= bounds[1] &&
+    coords.y <= bounds[3]
+  );
+}
+
+function checkElementClicked(e)
+{
+    var clickableElements = [run1Button, run2Button, run3Button]
+    for (var i=0; i<clickableElements.length; i++)
+    {
+        var testableElement = clickableElements[i]
+        var boundingBox = testableElement.getBBox();
+        var boundingCoords = [boundingBox.x, boundingBox.y, boundingBox.x+boundingBox.width, boundingBox.y+boundingBox.height]
+
+        const providedCoords = { x: e.screenX, y: e.screenY };
+
+        if (isWithinBounds(providedCoords, boundingCoords)) {
+          return testableElement
+        }
+    }
+}
+
+function checkDirection(e) {
+    touchendX = e.screenX
+  if ((touchendX - touchstartX) < -10)
+  {
+    if ((currentWeekIndex + 1) < weeks.length)
+    {
+        currentWeekIndex += 1;
+    }
+    myLabel.text = weeks[currentWeekIndex]["name"]
+  }
+  else if ((touchendX - touchstartX) > 10)
+  {
+    if ((currentWeekIndex - 1) >= 0)
+    {
+        currentWeekIndex -= 1;
+    }
+    myLabel.text = weeks[currentWeekIndex]["name"]
+  }
+  else
+  {
+      switch (checkElementClicked(e))
+    {
+        case run1Button:
+            weekDistributor(currentWeekIndex, 0)
+            break;
+        case run2Button:
+            weekDistributor(currentWeekIndex, 1)
+            break;
+        case run3Button:
+            weekDistributor(currentWeekIndex, 2)
+            break;
+    }
+  }
+}
+
+function handleClick(e)
+{
+    console.log("clicked!")
+    console.log(e.screenX, e.screenY)
+}
+
+tehs.addEventListener('mousedown', e => {
+    touchstartX = e.screenX
+})
+
+tehs.addEventListener('mouseup', e => {
+    checkDirection(e)
+})
--- app/runMethods.js
+++ app/runMethods.js
@@ -17,20 +17,201 @@ function event(type)
 }
 
 export async function week_1() {
-    await sleep(1000);
     event("Warm-Up Walk")
+    await sleep(300000);
 
     for (var i = 1; i <= 8; i++) {
-        await sleep(2000);
         event("Run " + i.toString())
+        await sleep(60000);
 
-        await sleep(2000);
         event("Walk")
+        await sleep(90000);
     }
+    event("Run " + i.toString())
+    await sleep(60000);
 
-    await sleep(2000);
     event("Cool-Down Walk")
+    await sleep(300000);
 
-    await sleep(2000);
     event("Finished!")
+}
+
+
+// *-*-* COMMAND DISTRIBUTOR *-*-*
+export async function weekDistributor(week, run) {
+    const functionName = `w${week + 1}r${run + 1}`;
+    console.log(functionName)
+    await executeFunction(functionName);
+}
+
+async function executeFunction(functionName) {
+    if (typeof this[functionName] === 'function') {
+        await this[functionName]();
+    } else {
+        console.log(`Function ${functionName} not found.`);
+    }
+}
+
+
+
+// *-*-* RUN METHODS *-*-*
+
+// Week 1
+async function w1r1()
+{
+console.log("week 1 - run 1");
+}
+
+async function w1r2()
+{
+console.log("week 1 - run 2");
+}
+
+async function w1r3()
+{
+console.log("week 1 - run 3");
+}
+
+
+
+// Week 2
+function w2r1()
+{
+console.log("week 2 - run 1");
+}
+
+function w2r2()
+{
+console.log("week 2 - run 2");
+}
+
+function w2r3()
+{
+console.log("week 2 - run 3");
+}
+
+
+
+// Week 3
+function w3r1()
+{
+console.log("week 3 - run 1");
+}
+
+function w3r2()
+{
+console.log("week 3 - run 2");
+}
+
+function w3r3()
+{
+console.log("week 3 - run 3");
+}
+
+
+
+// Week 4
+function w4r1()
+{
+console.log("week 4 - run 1");
+}
+
+function w4r2()
+{
+console.log("week 4 - run 2");
+}
+
+function w4r3()
+{
+console.log("week 4 - run 3");
+}
+
+
+
+// Week 5
+function w5r1()
+{
+console.log("week 5 - run 1");
+}
+
+function w5r2()
+{
+console.log("week 5 - run 2");
+}
+
+function w5r3()
+{
+console.log("week 5 - run 3");
+}
+
+
+
+// Week 6
+function w6r1()
+{
+console.log("week 6 - run 1");
+}
+
+function w6r2()
+{
+console.log("week 6 - run 2");
+}
+
+function w6r3()
+{
+console.log("week 6 - run 3");
+}
+
+
+
+// Week 7
+function w7r1()
+{
+console.log("week 7 - run 1");
+}
+
+function w7r2()
+{
+console.log("week 7 - run 2");
+}
+
+function w7r3()
+{
+console.log("week 7 - run 3");
+}
+
+
+
+// Week 8
+function w8r1()
+{
+console.log("week 8 - run 1");
+}
+
+function w8r2()
+{
+console.log("week 8 - run 2");
+}
+
+function w8r3()
+{
+console.log("week 8 - run 3");
+}
+
+
+
+// Week 9
+function w9r1()
+{
+console.log("week 9 - run 1");
+}
+
+function w9r2()
+{
+console.log("week 9 - run 2");
+}
+
+function w9r3()
+{
+console.log("week 9 - run 3");
 }
