54fb112852f65691f5168a094166f572bdde11ab
Gregor <gregor.hastings@outlook.com>
2024-02-25 21:53:37 +0000

    Fixed stack overflow

--- app/runMethods.js
+++ app/runMethods.js
@@ -6,15 +6,15 @@ const runInstructionText = document.getElementById("runInstructionText");
 var vibrationType = "nudge-max"
 
 const weeks = [
-    [w1r1, w1r2, w1r3],
-    [w2r1, w2r2, w2r3],
-    [w3r1, w3r2, w3r3],
-    [w4r1, w4r2, w4r3],
+    [w1r1, w1r1, w1r1],
+    [w2r1, w2r1, w2r1],
+    [w3r1, w3r1, w3r1],
+    [w4r1, w4r1, w4r1],
     [w5r1, w5r2, w5r3],
     [w6r1, w6r2, w6r3],
-    [w7r1, w7r2, w7r3],
-    [w8r1, w8r2, w8r3],
-    [w9r1, w9r2, w9r3],
+    [w6r3, w6r3, w6r3],
+    [w8r1, w8r1, w8r1],
+    [w9r1, w9r1, w9r1],
 ];
 
 
@@ -43,6 +43,7 @@ export async function weekRunDistributor(week, run) {
 
 // Week 1
 export async function w1r1() {
+    console.log("RUN 1")
     await event("Warm-Up Walk", 300)
 
     for (var i = 1; i <= 8; i++) {
@@ -55,14 +56,6 @@ export async function w1r1() {
     await event("Cool-Down Walk", 300)
 
     await event("Finished!", 0)
-}
-
-export async function w1r2() {
-    await w1r1();
-}
-
-export async function w1r3() {
-    await w1r1();
 }
 
 // Week 2
@@ -81,14 +74,6 @@ export async function w2r1() {
     await event("Finished!", 0)
 }
 
-export async function w2r2() {
-    await w2r1()
-}
-
-export async function w2r3() {
-    await w2r1()
-}
-
 // Week 3
 export async function w3r1() {
     await event("Warm-Up Walk", 300)
@@ -105,13 +90,6 @@ export async function w3r1() {
     await event("Finished!", 0)
 }
 
-export async function w3r2() {
-    await w3r1
-}
-
-export async function w3r3() {
-    await w3r1
-}
 
 // Week 4
 export async function w4r1() {
@@ -127,14 +105,6 @@ export async function w4r1() {
 
     await event("Cool-Down Walk", 300)
     await event("Finished!", 0)
-}
-
-export async function w4r2() {
-    await w4r1()
-}
-
-export async function w4r3() {
-    await w4r1()
 }
 
 // Week 5
@@ -205,19 +175,6 @@ export async function w6r3() {
     await event("Finished!", 0)
 }
 
-// Week 7
-export async function w7r1() {
-    await w6r3()
-}
-
-export async function w7r2() {
-    await w6r3()
-}
-
-export async function w7r3() {
-    await w6r3()
-}
-
 // Week 8
 export async function w8r1() {
     await event("Warm-Up Walk", 300)
@@ -228,13 +185,6 @@ export async function w8r1() {
     await event("Finished!", 0)
 }
 
-export async function w8r2() {
-    await w8r1()
-}
-
-export async function w8r3() {
-    await w8r1()
-}
 
 // Week 9
 export async function w9r1() {
@@ -244,12 +194,4 @@ export async function w9r1() {
 
     await event("Cool-Down Walk", 300)
     await event("Finished!", 0)
-}
-
-export async function w9r2() {
-    await w9r1();
-}
-
-export async function w9r3() {
-    await w9r1();
 }
